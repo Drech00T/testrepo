313af84cb7ab3b771093c77e440139ff951a6721
Drech00T <drechsler-thomas@gmx.net>
2018-07-22 20:41:26 +0200

    Initial Unity ECS Setup

---
+++ Beta ECS Try/Assets/Scripts/Systems/CubeSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class CubeSystem : ComponentSystem {
+
+    private struct Filter
+    {
+        public Rigidbody rb;
+        public CubeComponent cubeComp;
+        public Transform trans;
+        public OrbitComp orbitComp;
+    }
+
+	protected override void OnUpdate () {
+        foreach (var entity in GetEntities<Filter>())
+        {
+            Debug.Log(entity.rb.gameObject.name);
+            entity.trans.RotateAround(entity.orbitComp.rotationPivotPoint,Vector3.up,10);
+        }
+	}
+}
---
+++ Beta ECS Try/Assets/Scripts/Systems/LockRotationSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+public class LockRotationSystem : ComponentSystem {
+    private struct Filter
+    {
+        private Vector3 Rotation;
+        private CubeComponent cubeComp;
+    }
+
+	protected override void OnUpdate () {
+        foreach (var entity in GetEntities<Filter>())
+        {
+            entity.   entity.Rotation
+        }
+    }
+}   
